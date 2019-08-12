faffed2120bdf470f9975af46d9fd79b79e84354
gaithmtiri@gmail.com <gaithmtiri@gmail.com>
2019-08-12 14:13:03 -0400

    add image build

--- build-images.sh
+++ build-images.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-docker build -t spark-base:latest ./docker/base
-docker build -t spark-master:latest ./docker/spark-master
-docker build -t spark-worker:latest ./docker/spark-worker
+docker build -t spark-base:2.3.1 ./docker/base
+docker build -t spark-master:2.3.1 ./docker/spark-master
+docker build -t spark-worker:2.3.1 ./docker/spark-worker
+docker build -t spark-driver:2.3.1 ./docker/spark-driver/drf_app
+docker build -t spark-driver-spring:2.3.1 ./docker/spark-driver-spring
